324e8ad5360586e6c528932c053755c8e43a9f66
AdacusG <greenead@dickionson.edu>
2025-04-10 10:05:08 -0400

    Completed first test section.

--- farmdata2/farmdata2_modules/fd2_barn_kit/transplantingReport/transplantingReport.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/transplantingReport/transplantingReport.defaults.spec.js
@@ -7,15 +7,20 @@ describe("Testing Transplanting Report Default Values", () => {
         cy.waitForPage()
     })
 
+    // Adacus Section
     it("Checking if page is correct", () => {
         cy.get('[data-cy=transplant-report-header]')
+            .should("have.text", "Transplanting Report")
+        cy.get('[data-cy=set-date-legend]')
+            .should('exist')
     })
 
+    // Brian Section
     it("Testing default values", () => {
 
     })
 
     it("Checking Button and if the report is not visible.", () => {
-        
+
     })
 })
