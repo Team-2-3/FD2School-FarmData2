64f001c1d689afbf0019ee9a14f630f70e7f995b
AdacusG <greenead@dickionson.edu>
2025-04-24 09:26:59 -0400

    Completed Section and wrote Skeleton Code for other sections.

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.testCol.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.testCol.spec.js
@@ -5,19 +5,52 @@ describe("Testing Transplanting Report Default Values", () => {
 
         // Wait here for the maps to load in the page.
         cy.waitForPage()
+
+        cy.get('[data-cy=start-date-select]')
+            .type('2020-01-01')
+
+        cy.get('[data-cy="generate-rpt-btn"]').click()
     })
 
     // Adacus section
     it("Check if the columns values are correct when All is selected", () => {
-        
-    })
+        const expectedColumns = [
+            'Date',
+            'Crop',
+            'Area',
+            'Seeding',
+            'Workers',
+            'Hours',
+            'Varieties',
+            'Comments',
+            'User',
+            'Edit'
+        ]
+        //Check if columns are visible.
+        expectedColumns.forEach(column => {
+            cy.get('[data-cy="table-headers"]').contains(column).should('be.visible')
+        })
 
+        const hiddenColumns = [
+            'Row Feet',
+            'Bed Feet',
+            'Rows/Bed',
+            'Seeds',
+            'Trays',
+            'Cells/Tray'
+        ]
+        //Check if hidden columns DNE.
+        hiddenColumns.forEach(column => {
+            cy.get('[data-cy="table-headers"]').contains(column).should('not.exist')
+        })
+
+    })
+    
     // Brian section
     it("Check if the columns values are correct when Direct Seedings is selected", () => {
         //Select "Direct Seedings" from the seeding type dropdown
-        cy.get('[data-cy="seeding-type-dropdown"]').click()
-        cy.contains('li', 'Direct Seedings').click()
-
+        cy.get('[data-cy="seeding-type-dropdown"] > [data-cy="dropdown-input"]')
+        
         // Verify the visible columns
         const expectedColumns = [
             'Select',
@@ -38,7 +71,7 @@ describe("Testing Transplanting Report Default Values", () => {
 
         // Check each expected column exists in the table header
         expectedColumns.forEach(column => {
-            cy.get('th').contains(column).should('be.visible')
+            cy.get('[data-cy="table-headers"]').contains(column).should('be.visible')
         })
 
         // Verify columns that should NOT be visible for Direct Seedings
@@ -49,14 +82,46 @@ describe("Testing Transplanting Report Default Values", () => {
         ]
 
         hiddenColumns.forEach(column => {
-            cy.get('th').contains(column).should('not.exist')
+            cy.get('[data-cy="table-headers"]').contains(column).should('not.exist')
         })
         
     })
 
     // Aziz section
     it("Check if the columns values are correct when Tray Seeding is selected", () => {
+        cy.get('[data-cy="seeding-type-dropdown"] > [data-cy="dropdown-input"] > [data-cy="option1"]').click()
         
+        const expectedColumns = [
+            'Date',
+            'Crop',
+            'Area',
+            'Seeding',
+            'Seeds',
+            'Trays',
+            'Cells/Tray',
+            'Workers',
+            'Hours',
+            'Varieties',
+            'Comments',
+            'User',
+            'Edit'
+        ]
+
+        //Check each expected column exists in the table header
+        expectedColumns.forEach(column => {
+            cy.get('[data-cy="table-headers"]').contains(column).should('be.visible')
+        })
+        
+        const hiddenColumns = [
+            'Row Feet',
+            'Bed Feet',
+            'Rows/Bed'
+        ]
+
+        // Verify columns that should NOT be visible
+        hiddenColumns.forEach(column => {
+            cy.get('[data-cy="table-headers"]').contains(column).should('not.exist')
+        })
     })
 
 })
