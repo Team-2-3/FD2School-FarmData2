803d7e3b46ec5536e7752d39d397b77ba7daa67e
AdacusG <greenead@dickionson.edu>
2025-04-24 10:08:34 -0400

    Created skeleton code for issue 30

---
+++ farmdata2/farmdata2_modules/fd2_barn_kit/cypress/barnkit.subtab.spec.js
@@ -0,0 +1,18 @@
+describe("Test for BarnKit Sub-Tabs", () => {
+    beforeEach(() => {
+
+    })
+
+    // Adacus Section
+    it("The BarnKit tab contains sub-tabs", () => {
+
+    })
+    // Brian Section
+    it("The order of the tabs are correct", () => {
+
+    })
+    // Aziz Section
+    it("The number of sub-tabs are correct", () => {
+        
+    })
+})
